f3f22863b4bdbc973294394047a699e00e2441c5
Pistol97 <ehfdl75@gmail.com>
2020-11-04 12:17:25 +0900

    Setup Project

---
+++ Source/ProjectBreakfast/ProjectBreakfastGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectBreakfastGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AProjectBreakfastGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AProjectBreakfastGameMode();
+};
+
+
+
